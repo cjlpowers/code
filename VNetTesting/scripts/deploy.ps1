437c85bb925d39de7f47836eccf3c65ef992f56a
Chris Powers <cpowers@outlook.com>
2019-01-13 21:48:04 -0600

    Implement Dockerfile

--- VNetTesting/scripts/deploy.ps1
+++ VNetTesting/scripts/deploy.ps1
@@ -7,7 +7,10 @@ param(
     [String]$Location = "South Central US"
 )
 process{
+    "Creating resource group..."
     $resourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
     $resourceGroup | Format-List
+
+    "Removing resource group..."
     $resourceGroup | Remove-AzResourceGroup -Force
 }
