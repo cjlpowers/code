16373013f0c9a959aa237b56bb48969abec2c41b
Chris Powers <cpowers@outlook.com>
2019-01-20 17:16:33 -0600

    Initial commit (WIP)

---
+++ raspberry-pi/RPiBackyardAutomation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace RPiBackyardAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {   
+                var pin = Pi.Gpio[0];
+                pin.PinMode = GpioPinDriveMode.Input;
+
+                while(true)
+                {
+                    var pinValue = pin.Read();
+                    Console.WriteLine($"{pin.Name} = {pinValue}");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
