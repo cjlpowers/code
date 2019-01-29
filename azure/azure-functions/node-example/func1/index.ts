9eea7e5a5671215495f9fe0642b5098d6a3f16f9
Chris Powers <cpowers@outlook.com>
2019-01-29 16:09:05 -0600

    Remove azure functions example

--- azure/azure-functions/node-example/func1/index.ts
+++
@@ -1,8 +0,0 @@
-import { IHttpContext, HttpRequest } from "azure-functions-typedefinitions";
-
-export async function run(context: IHttpContext, req: HttpRequest) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res.status = 200;
-    context.res.body = "Hello "+ Date.now();
-};
