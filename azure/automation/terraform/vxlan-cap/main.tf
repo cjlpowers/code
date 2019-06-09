a9de0586608572ce745e3e322c598b02b652bc58
Chris Powers <cpowers@outlook.com>
2019-06-08 22:09:36 -0500

    Add main.tf

---
+++ azure/automation/terraform/vxlan-cap/main.tf
@@ -0,0 +1,19 @@
+################################
+# Variables
+################################
+
+################################
+# Provider
+################################
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.28.0"
+}
+
+################################
+# Resources
+################################
+
+################################
+# Outputs
+################################
