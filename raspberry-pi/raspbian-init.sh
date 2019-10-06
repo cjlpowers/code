25a6836e853d448b1e94526543056c7cd1fb5e23
Chris Powers <cpowers@outlook.com>
2019-10-06 12:57:48 -0500

    Add comments

--- raspberry-pi/raspbian-init.sh
+++ raspberry-pi/raspbian-init.sh
@@ -1,6 +1,7 @@
+# show commands
 set -x
 
-# update 
+# install updates
 apt-get update && sudo apt-get -y upgrade
 
 # install docker
@@ -8,4 +9,5 @@ curl -sSL https://get.docker.com | sh
 usermod -aG docker pi
 service docker restart
 
+# install rpi-monitor - https://github.com/michaelmiklis/docker-rpi-monitor
 docker run --restart always --device=/dev/vchiq --device=/dev/vcsm --volume=/opt/vc:/opt/vc --volume=/boot:/boot --volume=/sys:/dockerhost/sys:ro --volume=/etc:/dockerhost/etc:ro --volume=/proc:/dockerhost/proc:ro --volume=/usr/lib:/dockerhost/usr/lib:ro -p=8888:8888 --name="rpi-monitor" -d michaelmiklis/rpi-monitor:latest
